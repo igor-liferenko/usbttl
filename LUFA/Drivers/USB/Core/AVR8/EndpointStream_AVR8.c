d7645de1c0dd80ea358755e3851ee86bec908abe
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-05 10:36:20 +0700

    edit

--- LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
+++ LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
@@ -234,13 +234,53 @@ uint8_t Endpoint_Write_Stream_BE(const void* const Buffer,
 	#define  TEMPLATE_TRANSFER_BYTE(BufferPtr)         Endpoint_Write_8(eeprom_read_byte(BufferPtr))
 	#include "Template/Template_Endpoint_RW.c"
 
-	#define  TEMPLATE_FUNC_NAME                        Endpoint_Read_EStream_LE
-	#define  TEMPLATE_BUFFER_TYPE                      void*
-	#define  TEMPLATE_CLEAR_ENDPOINT()                 Endpoint_ClearOUT()
-	#define  TEMPLATE_BUFFER_OFFSET(Length)            0
-	#define  TEMPLATE_BUFFER_MOVE(BufferPtr, Amount)   BufferPtr += Amount
-	#define  TEMPLATE_TRANSFER_BYTE(BufferPtr)         eeprom_update_byte(BufferPtr, Endpoint_Read_8())
-	#include "Template/Template_Endpoint_RW.c"
+uint8_t Endpoint_Read_EStream_LE(void* const Buffer,
+                            uint16_t Length,
+                            uint16_t* const BytesProcessed)
+{
+	uint8_t* DataStream      = ((uint8_t*)Buffer + 0);
+	uint16_t BytesInTransfer = 0;
+	uint8_t  ErrorCode;
+
+	if ((ErrorCode = Endpoint_WaitUntilReady()))
+	  return ErrorCode;
+
+	if (BytesProcessed != NULL)
+	{
+		Length -= *BytesProcessed;
+		DataStream += *BytesProcessed;
+	}
+
+	while (Length)
+	{
+		if (!(Endpoint_IsReadWriteAllowed()))
+		{
+			Endpoint_ClearOUT();
+
+			#if !defined(INTERRUPT_CONTROL_ENDPOINT)
+			USB_USBTask();
+			#endif
+
+			if (BytesProcessed != NULL)
+			{
+				*BytesProcessed += BytesInTransfer;
+				return ENDPOINT_RWSTREAM_IncompleteTransfer;
+			}
+
+			if ((ErrorCode = Endpoint_WaitUntilReady()))
+			  return ErrorCode;
+		}
+		else
+		{
+			eeprom_update_byte(DataStream, Endpoint_Read_8());
+			DataStream += 1;
+			Length--;
+			BytesInTransfer++;
+		}
+	}
+
+	return ENDPOINT_RWSTREAM_NoError;
+}
 
 uint8_t Endpoint_Read_EStream_BE(void* const Buffer,
                             uint16_t Length,
