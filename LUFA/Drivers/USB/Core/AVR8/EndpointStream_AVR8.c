e539cf30c7c632127e26e20dcbc43ece94d87039
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-05 10:13:56 +0700

    edit

--- LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
+++ LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.c
@@ -303,11 +303,49 @@ uint8_t Endpoint_Read_Control_Stream_BE(void* const Buffer, uint16_t Length)
 	#define  TEMPLATE_TRANSFER_BYTE(BufferPtr)         Endpoint_Write_8(eeprom_read_byte(BufferPtr))
 	#include "Template/Template_Endpoint_Control_W.c"
 
-	#define  TEMPLATE_FUNC_NAME                        Endpoint_Read_Control_EStream_LE
-	#define  TEMPLATE_BUFFER_OFFSET(Length)            0
-	#define  TEMPLATE_BUFFER_MOVE(BufferPtr, Amount)   BufferPtr += Amount
-	#define  TEMPLATE_TRANSFER_BYTE(BufferPtr)         eeprom_update_byte(BufferPtr, Endpoint_Read_8())
-	#include "Template/Template_Endpoint_Control_R.c"
+uint8_t Endpoint_Read_Control_EStream_LE(void* const Buffer, uint16_t Length)
+{
+	uint8_t* DataStream = ((uint8_t*)Buffer + 0);
+
+	if (!(Length))
+	  Endpoint_ClearOUT();
+
+	while (Length)
+	{
+		uint8_t USB_DeviceState_LCL = USB_DeviceState;
+
+		if (USB_DeviceState_LCL == DEVICE_STATE_Unattached)
+		  return ENDPOINT_RWCSTREAM_DeviceDisconnected;
+		else if (USB_DeviceState_LCL == DEVICE_STATE_Suspended)
+		  return ENDPOINT_RWCSTREAM_BusSuspended;
+		else if (Endpoint_IsSETUPReceived())
+		  return ENDPOINT_RWCSTREAM_HostAborted;
+
+		if (Endpoint_IsOUTReceived())
+		{
+			while (Length && Endpoint_BytesInEndpoint())
+			{
+				eeprom_update_byte(DataStream, Endpoint_Read_8());
+				DataStream += 1;
+				Length--;
+			}
+
+			Endpoint_ClearOUT();
+		}
+	}
+
+	while (!(Endpoint_IsINReady()))
+	{
+		uint8_t USB_DeviceState_LCL = USB_DeviceState;
+
+		if (USB_DeviceState_LCL == DEVICE_STATE_Unattached)
+		  return ENDPOINT_RWCSTREAM_DeviceDisconnected;
+		else if (USB_DeviceState_LCL == DEVICE_STATE_Suspended)
+		  return ENDPOINT_RWCSTREAM_BusSuspended;
+	}
+
+	return ENDPOINT_RWCSTREAM_NoError;
+}
 
 uint8_t Endpoint_Read_Control_EStream_BE(void* const Buffer, uint16_t Length)
 {
