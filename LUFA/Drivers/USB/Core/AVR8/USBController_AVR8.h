d73af58c0ac5ffefb4c0ca0fe4e39fccac48677a
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-09 16:04:30 +0700

    fix "get rid of USBController_AVR8.c"

--- LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h
+++ LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h
@@ -1,33 +1,3 @@
-/*
-             LUFA Library
-     Copyright (C) Dean Camera, 2017.
-
-  dean [at] fourwalledcubicle [dot] com
-           www.lufa-lib.org
-*/
-
-/*
-  Copyright 2017  Dean Camera (dean [at] fourwalledcubicle [dot] com)
-
-  Permission to use, copy, modify, distribute, and sell this
-  software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in
-  all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
-  software without specific, written prior permission.
-
-  The author disclaims all warranties with regard to this
-  software, including all implied warranties of merchantability
-  and fitness.  In no event shall the author be liable for any
-  special, indirect or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether
-  in an action of contract, negligence or other tortious action,
-  arising out of or in connection with the use or performance of
-  this software.
-*/
-
 /** \file
  *  \brief USB Controller definitions for the AVR8 microcontrollers.
  *  \copydetails Group_USBManagement_AVR8
@@ -55,64 +25,19 @@
 		#include "../USBTask.h"
 		#include "../USBInterrupt.h"
 
-		#if defined(USB_CAN_BE_HOST) || defined(__DOXYGEN__)
-			#include "../Host.h"
-			#include "../OTG.h"
-			#include "../Pipe.h"
-			#include "../HostStandardReq.h"
-			#include "../PipeStream.h"
-		#endif
 
-		#if defined(USB_CAN_BE_DEVICE) || defined(__DOXYGEN__)
 			#include "../Device.h"
 			#include "../Endpoint.h"
 			#include "../DeviceStandardReq.h"
 			#include "../EndpointStream.h"
-		#endif
 
-	/* Enable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			extern "C" {
-		#endif
 
 	/* Preprocessor Checks and Defines: */
 		#if !defined(__INCLUDE_FROM_USB_DRIVER)
 			#error Do not include this file directly. Include LUFA/Drivers/USB/USB.h instead.
 		#endif
 
-		#if !defined(F_USB)
-			#error F_USB is not defined. You must define F_USB to the frequency of the unprescaled USB controller clock in your project makefile.
-		#endif
-
-		#if (F_USB == 8000000)
-			#if (defined(__AVR_AT90USB82__) || defined(__AVR_AT90USB162__) || \
-			     defined(__AVR_ATmega8U2__) || defined(__AVR_ATmega16U2__) || \
-			     defined(__AVR_ATmega32U2__))
-				#define USB_PLL_PSC                0
-			#elif (defined(__AVR_ATmega16U4__) || defined(__AVR_ATmega32U4__))
-				#define USB_PLL_PSC                0
-			#elif (defined(__AVR_AT90USB646__)  || defined(__AVR_AT90USB1286__))
-				#define USB_PLL_PSC                ((1 << PLLP1) | (1 << PLLP0))
-			#elif (defined(__AVR_AT90USB647__)  || defined(__AVR_AT90USB1287__))
-				#define USB_PLL_PSC                ((1 << PLLP1) | (1 << PLLP0))
-			#endif
-		#elif (F_USB == 16000000)
-			#if (defined(__AVR_AT90USB82__) || defined(__AVR_AT90USB162__) || \
-			     defined(__AVR_ATmega8U2__) || defined(__AVR_ATmega16U2__) || \
-			     defined(__AVR_ATmega32U2__))
-				#define USB_PLL_PSC                (1 << PLLP0)
-			#elif (defined(__AVR_ATmega16U4__) || defined(__AVR_ATmega32U4__))
-				#define USB_PLL_PSC                (1 << PINDIV)
-			#elif (defined(__AVR_AT90USB646__) || defined(__AVR_AT90USB647__))
-				#define USB_PLL_PSC                ((1 << PLLP2) | (1 << PLLP1))
-			#elif (defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB1287__))
-				#define USB_PLL_PSC                ((1 << PLLP2) | (1 << PLLP0))
-			#endif
-		#endif
-
-		#if !defined(USB_PLL_PSC)
-			#error No PLL prescale value available for chosen F_USB value and AVR model.
-		#endif
+		#define USB_PLL_PSC                (1 << PINDIV)
 
 	/* Public Interface - May be used in end-application: */
 		/* Macros: */
@@ -154,7 +79,6 @@
 			#define USB_OPT_AUTO_PLL                   (0 << 2)
 			//@}
 
-			#if !defined(USB_STREAM_TIMEOUT_MS) || defined(__DOXYGEN__)
 				/** Constant for the maximum software timeout period of the USB data stream transfer functions
 				 *  (both control and standard) when in either device or host mode. If the next packet of a stream
 				 *  is not received or acknowledged within this time period, the stream function will fail.
@@ -163,10 +87,9 @@
 				 *  \ref USB_STREAM_TIMEOUT_MS token, and passed to the compiler using the -D switch.
 				 */
 				#define USB_STREAM_TIMEOUT_MS       100
-			#endif
 
 		/* Inline Functions: */
-			#if defined(USB_SERIES_4_AVR) || defined(USB_SERIES_6_AVR) || defined(USB_SERIES_7_AVR) || defined(__DOXYGEN__)
+
 				/** Determines if the VBUS line is currently high (i.e. the USB host is supplying power).
 				 *
 				 *  \note This function is not available on some AVR models which do not support hardware VBUS monitoring.
@@ -178,7 +101,6 @@
 				{
 					return ((USBSTA & (1 << VBUS)) ? true : false);
 				}
-			#endif
 
 			/** Detaches the device from the USB bus. This has the effect of removing the device from any
 			 *  attached host, ceasing USB communications. If no host is present, this prevents any host from
@@ -249,21 +171,7 @@
 			 *
 			 *  \see \ref Group_Device for the \c USB_DEVICE_OPT_* masks.
 			 */
-			void USB_Init(
-			               #if defined(USB_CAN_BE_BOTH) || defined(__DOXYGEN__)
-			               const uint8_t Mode
-			               #endif
-
-			               #if (defined(USB_CAN_BE_BOTH) && !defined(USE_STATIC_OPTIONS)) || defined(__DOXYGEN__)
-			               ,
-			               #elif (!defined(USB_CAN_BE_BOTH) && defined(USE_STATIC_OPTIONS))
-			               void
-			               #endif
-
-			               #if !defined(USE_STATIC_OPTIONS) || defined(__DOXYGEN__)
-			               const uint8_t Options
-			               #endif
-			               );
+			void USB_Init(void);
 
 			/** Shuts down the USB interface. This turns off the USB interface after deallocating all USB FIFO
 			 *  memory, endpoints and pipes. When turned off, no USB functionality can be used until the interface
@@ -277,32 +185,9 @@
 			void USB_ResetInterface(void);
 
 		/* Global Variables: */
-			#if !defined(USE_STATIC_OPTIONS) || defined(__DOXYGEN__)
-				/** Indicates the current USB options that the USB interface was initialized with when \ref USB_Init()
-				 *  was called. This value will be one of the \c USB_MODE_* masks defined elsewhere in this module.
-				 *
-				 *  \attention This variable should be treated as read-only in the user application, and never manually
-				 *             changed in value.
-				 */
-				extern volatile uint8_t USB_Options;
-			#elif defined(USE_STATIC_OPTIONS)
-				#define USB_Options USE_STATIC_OPTIONS
-			#endif
 
-	/* Private Interface - For use in library only: */
-	#if !defined(__DOXYGEN__)
-		/* Function Prototypes: */
-			#if defined(__INCLUDE_FROM_USB_CONTROLLER_C)
-				#if defined(USB_CAN_BE_DEVICE)
-				static void USB_Init_Device(void);
-				#endif
+		#define USB_Options USE_STATIC_OPTIONS
 
-				#if defined(USB_CAN_BE_HOST)
-				static void USB_Init_Host(void);
-				#endif
-			#endif
-
-		/* Inline Functions: */
 			static inline void USB_PLL_On(void) ATTR_ALWAYS_INLINE;
 			static inline void USB_PLL_On(void)
 			{
@@ -325,24 +210,15 @@
 			static inline void USB_REG_On(void) ATTR_ALWAYS_INLINE;
 			static inline void USB_REG_On(void)
 			{
-			#if defined(USB_SERIES_4_AVR) || defined(USB_SERIES_6_AVR) || defined(USB_SERIES_7_AVR)
 				UHWCON |=  (1 << UVREGE);
-			#else
-				REGCR  &= ~(1 << REGDIS);
-			#endif
 			}
 
 			static inline void USB_REG_Off(void) ATTR_ALWAYS_INLINE;
 			static inline void USB_REG_Off(void)
 			{
-			#if defined(USB_SERIES_4_AVR) || defined(USB_SERIES_6_AVR) || defined(USB_SERIES_7_AVR)
 				UHWCON &= ~(1 << UVREGE);
-			#else
-				REGCR  |=  (1 << REGDIS);
-			#endif
 			}
 
-			#if defined(USB_SERIES_4_AVR) || defined(USB_SERIES_6_AVR) || defined(USB_SERIES_7_AVR)
 			static inline void USB_OTGPAD_On(void) ATTR_ALWAYS_INLINE;
 			static inline void USB_OTGPAD_On(void)
 			{
@@ -354,7 +230,6 @@
 			{
 				USBCON &= ~(1 << OTGPADE);
 			}
-			#endif
 
 			static inline void USB_CLK_Freeze(void) ATTR_ALWAYS_INLINE;
 			static inline void USB_CLK_Freeze(void)
@@ -386,24 +261,6 @@
 				USBCON &= ~(1 << USBE);
 				USBCON |=  (1 << USBE);
 			}
-
-			#if defined(USB_CAN_BE_BOTH)
-			static inline uint8_t USB_GetUSBModeFromUID(void) ATTR_WARN_UNUSED_RESULT ATTR_ALWAYS_INLINE;
-			static inline uint8_t USB_GetUSBModeFromUID(void)
-			{
-				if (USBSTA & (1 << ID))
-				  return USB_MODE_Device;
-				else
-				  return USB_MODE_Host;
-			}
-			#endif
-
-	#endif
-
-	/* Disable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			}
-		#endif
 
 #endif
 
