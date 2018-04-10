fbbe609498a69685a474c0c5152e7270b19a4b87
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-10 14:26:00 +0700

    edit

--- LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.h
+++ LUFA/Drivers/USB/Core/AVR8/EndpointStream_AVR8.h
@@ -1,33 +1,3 @@
-/*
-             LUFA Library
-     Copyright (C) Dean Camera, 2017.
-
-  dean [at] fourwalledcubicle [dot] com
-           www.lufa-lib.org
-*/
-
-/*
-  Copyright 2017  Dean Camera (dean [at] fourwalledcubicle [dot] com)
-
-  Permission to use, copy, modify, distribute, and sell this
-  software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in
-  all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
-  software without specific, written prior permission.
-
-  The author disclaims all warranties with regard to this
-  software, including all implied warranties of merchantability
-  and fitness.  In no event shall the author be liable for any
-  special, indirect or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether
-  in an action of contract, negligence or other tortious action,
-  arising out of or in connection with the use or performance of
-  this software.
-*/
-
 /** \file
  *  \brief Endpoint data stream transmission and reception management for the AVR8 microcontrollers.
  *  \copydetails Group_EndpointStreamRW_AVR8
@@ -53,11 +23,6 @@
 		#include "../../../../Common/Common.h"
 		#include "../USBMode.h"
 		#include "../USBTask.h"
-
-	/* Enable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			extern "C" {
-		#endif
 
 	/* Preprocessor Checks: */
 		#if !defined(__INCLUDE_FROM_USB_DRIVER)
@@ -646,11 +611,6 @@
 			uint8_t Endpoint_Write_Control_PStream_BE(const void* const Buffer,
 			                                          uint16_t Length) ATTR_NON_NULL_PTR_ARG(1);
 			//@}
-
-	/* Disable C linkage for C++ Compilers: */
-		#if defined(__cplusplus)
-			}
-		#endif
 
 #endif
 
