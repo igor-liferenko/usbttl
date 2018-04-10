d116f7cac1e1712df02754e581cbd29081bb8823
Igor Liferenko <igor.liferenko@gmail.com>
2018-04-10 15:38:41 +0700

    cleanup Platform.h

--- LUFA/Platform/Platform.h
+++ LUFA/Platform/Platform.h
@@ -1,33 +1,3 @@
-/*
-             LUFA Library
-     Copyright (C) Dean Camera, 2017.
-
-  dean [at] fourwalledcubicle [dot] com
-           www.lufa-lib.org
-*/
-
-/*
-  Copyright 2017  Dean Camera (dean [at] fourwalledcubicle [dot] com)
-
-  Permission to use, copy, modify, distribute, and sell this
-  software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in
-  all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
-  software without specific, written prior permission.
-
-  The author disclaims all warranties with regard to this
-  software, including all implied warranties of merchantability
-  and fitness.  In no event shall the author be liable for any
-  special, indirect or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether
-  in an action of contract, negligence or other tortious action,
-  arising out of or in connection with the use or performance of
-  this software.
-*/
-
 /** \file
  *  \brief Architecture Specific Hardware Platform Drivers.
  *
@@ -61,20 +31,3 @@
  *  \note The exact APIs and availability of sub-modules within the platform driver group may vary depending on the
  *        target used - see individual target module documentation for the API specific to your target processor.
  */
-
-#ifndef __LUFA_PLATFORM_H__
-#define __LUFA_PLATFORM_H__
-
-	/* Includes: */
-		#include "../Common/Common.h"
-
-	/* Includes: */
-		#if (ARCH == ARCH_UC3)
-			#include "UC3/ClockManagement.h"
-			#include "UC3/InterruptManagement.h"
-		#elif (ARCH == ARCH_XMEGA)
-			#include "XMEGA/ClockManagement.h"
-		#endif
-
-#endif
-
